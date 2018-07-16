3a2435f76453ac8b3fdb612b971c016557144e02
jmdressner <jmdressner@gmail.com>
2018-07-16 14:52:36 -0700

    added navbars for roles and added bootstrap and image

--- CrystalBallpro/Global.asax.cs
+++ CrystalBallpro/Global.asax.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using CrystalBallpro.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -18,6 +20,7 @@ namespace CrystalBallpro
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            GlobalConfiguration.Configuration.EnsureInitialized();
         }
     }
 }
