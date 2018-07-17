62e8b732fb00530066725c624b62370a73291b7c
John Vollmer <vollme22@uwm.edu>
2018-07-17 07:17:39 -0500

    Fixed merge conflicts

--- CrystalBallpro/Models/Employee.cs
+++ CrystalBallpro/Models/Employee.cs
@@ -8,7 +8,7 @@ using System.Web;
 namespace CrystalBallpro.Models
 {
     public class Employee
-
+    { 
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
