c48ed2d4ea2f220e00376f7b0dda5bf876e0495c
jmdressner <jmdressner@gmail.com>
2018-07-19 07:29:39 -0700

    fixed migrations issues

--- CrystalBallpro/Migrations/201807191427339_initial.Designer.cs
+++ CrystalBallpro/Migrations/201807191427339_initial.Designer.cs
@@ -7,13 +7,13 @@ namespace CrystalBallpro.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class salesinfo : IMigrationMetadata
+    public sealed partial class initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(salesinfo));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201807182110507_salesinfo"; }
+            get { return "201807191427339_initial"; }
         }
         
         string IMigrationMetadata.Source
---
+++ CrystalBallpro/Migrations/201807191427339_initial.cs
@@ -0,0 +1,232 @@
+namespace CrystalBallpro.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Admins",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Email = c.String(),
+                        ApplicationUserID = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.AspNetUsers", t => t.ApplicationUserID)
+                .Index(t => t.ApplicationUserID);
+            
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Role = c.String(),
+                        Email = c.String(maxLength: 256),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                    {
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+            
+            CreateTable(
+                "dbo.Availabilities",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        AdminID = c.Int(),
+                        EmployeeID = c.Int(),
+                        DayID = c.Int(nullable: false),
+                        StartTimeID = c.Int(nullable: false),
+                        EndTimeID = c.Int(nullable: false),
+                        WorkStatus = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Admins", t => t.AdminID)
+                .ForeignKey("dbo.Employees", t => t.EmployeeID)
+                .ForeignKey("dbo.EndTimes", t => t.EndTimeID, cascadeDelete: true)
+                .ForeignKey("dbo.StartTimes", t => t.StartTimeID, cascadeDelete: true)
+                .ForeignKey("dbo.Weeks", t => t.DayID, cascadeDelete: true)
+                .Index(t => t.AdminID)
+                .Index(t => t.EmployeeID)
+                .Index(t => t.DayID)
+                .Index(t => t.StartTimeID)
+                .Index(t => t.EndTimeID);
+            
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Email = c.String(),
+                        ApplicationUserID = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.ApplicationUserID)
+                .Index(t => t.ApplicationUserID);
+            
+            CreateTable(
+                "dbo.EndTimes",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        End = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.StartTimes",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Start = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Weeks",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Day = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Inventories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Stock = c.Int(nullable: false),
+                        Units = c.String(),
+                        Expiration = c.String(),
+                        LastOrdered = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+            
+            CreateTable(
+                "dbo.SalesHistoricals",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        NumberOfEvents = c.Int(nullable: false),
+                        NumberOfEmployees = c.Int(nullable: false),
+                        SalesIncreasePercent = c.Double(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Schedules",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.Availabilities", "DayID", "dbo.Weeks");
+            DropForeignKey("dbo.Availabilities", "StartTimeID", "dbo.StartTimes");
+            DropForeignKey("dbo.Availabilities", "EndTimeID", "dbo.EndTimes");
+            DropForeignKey("dbo.Availabilities", "EmployeeID", "dbo.Employees");
+            DropForeignKey("dbo.Employees", "ApplicationUserID", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Availabilities", "AdminID", "dbo.Admins");
+            DropForeignKey("dbo.Admins", "ApplicationUserID", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.Employees", new[] { "ApplicationUserID" });
+            DropIndex("dbo.Availabilities", new[] { "EndTimeID" });
+            DropIndex("dbo.Availabilities", new[] { "StartTimeID" });
+            DropIndex("dbo.Availabilities", new[] { "DayID" });
+            DropIndex("dbo.Availabilities", new[] { "EmployeeID" });
+            DropIndex("dbo.Availabilities", new[] { "AdminID" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            DropIndex("dbo.Admins", new[] { "ApplicationUserID" });
+            DropTable("dbo.Schedules");
+            DropTable("dbo.SalesHistoricals");
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.Inventories");
+            DropTable("dbo.Weeks");
+            DropTable("dbo.StartTimes");
+            DropTable("dbo.EndTimes");
+            DropTable("dbo.Employees");
+            DropTable("dbo.Availabilities");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.AspNetUsers");
+            DropTable("dbo.Admins");
+        }
+    }
+}
